a460be8019cf926c90d88d90ba37b26c0e32869d
Harshil Khamar <harshilkhamar1@gmail.com>
2022-03-10 00:52:42 +0530

    Foreground Animation Done

--- AIC/AIC_APP/templates/AIC_APP/bganimation.js
+++ AIC/AIC_APP/templates/AIC_APP/bganimation.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-    gsap.from("#Green_1_", { duration: 3, y: -200, rotation: 360, ease: "bounce" });
+    // gsap.from("#Green_1_", { duration: 3, y: -200, rotation: 360, ease: "bounce" });
+    gsap.from("#Green_1_", { duration: 1, rotation: 180, y: -200, scale: 0, ease: "ease.in" });
+    gsap.from("#purple_1_", { duration: 2, delay: 1, rotation: 180, x: -200, scale: 0, rotation: 180, ease: "back" });
+
+    gsap.timeline().from("#Gray_1_", { duration: 2, yPercent: 400, xPercent: -200, rotation: 180, opacity: 0, ease: "ease.in" })
+    gsap.from("#red_1_", { duration: 1, rotation: -180, opacity: 0, scale: 0.2, delay: 0.5 });
+    gsap.from("#blue2_1_", { duration: 1, rotation: -360, opacity: 0, scale: 0.2, delay: 0.5, ease: "stop" });
+    gsap.timeline().from("#bluemain_1_", { duration: 1.5, delay: 0.7, rotation: 360, scale: 0, opacity: 0, ease: "power4" });
+    gsap.timeline().from("#yellow_1_", { duration: 1.5, delay: 1.5, rotation: -360, scale: 2, opacity: 0, ease: "power4" });
+
 });
