5b21a89f08ebee016b03c40f61fce9cf72c6ea9d
siddhi <siddhikdoshi@gmail.com>
2022-03-13 17:24:44 +0530

    just for check

--- AIC/AIC_APP/views.py
+++ AIC/AIC_APP/views.py
@@ -9,8 +9,8 @@ from django.shortcuts import render
 from keras.models import load_model
 from nltk.stem import WordNetLemmatizer
 import os
-lemmatizer = WordNetLemmatizer()
 
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open(f"{os.getcwd()}\\AIC_APP\\training\\intents.json").read())
 words = pickle.load(open(f"{os.getcwd()}\\AIC_APP\\training\\words.pkl", 'rb'))
@@ -22,6 +22,8 @@ def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
+
+
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
@@ -30,6 +32,8 @@ def bag_of_words(sentence):
             if word == w:
                 bag[i] = 1
     return np.array(bag)
+
+
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
@@ -40,6 +44,8 @@ def predict_class(sentence):
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
+
+
 def get_response(intent_list, intent_json):
     tag = intent_list[0]['intent']
     # print(tag)
@@ -51,6 +57,8 @@ def get_response(intent_list, intent_json):
             break
 
     return result
+
+
 def index(request):
     return render(request, 'AIC_APP/background.html')
 
@@ -64,7 +72,6 @@ def takeOutput(request):
     return HttpResponse(res)
 
 
-
 # for handling the data given by the company xyz
 def fetchInputTextArea(request):
     inputText = request.POST.get('inputText', 'default')
@@ -75,6 +82,7 @@ def fetchInputTextArea(request):
     print('write')
     file.close()
     return HttpResponse("Success")
+
 
 def QueGenerator(request):
     return render(request, 'AIC_APP/questionGeneration.html')
