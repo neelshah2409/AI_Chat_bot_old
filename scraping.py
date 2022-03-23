c8a7987af70ef52a847aa6c956955813e150cb3a
Siddhi Doshi <siddhikdoshi@gmail.com>
2022-03-23 22:58:42 +0530

    final json ready

--- scraping.py
+++ scraping.py
@@ -36,44 +36,44 @@ import requests as r
 
 # Student Development Scheme
 ##########################################################
-# html_page = requests.get("https://www.aicte-india.org/schemes/students-development-schemes").text
-#
-# scrap = BeautifulSoup(html_page,"lxml")
-#
-# student_schemes = scrap.find("div",class_="schemes_html lzt_html")
-#
-# temp = ""
-# queAns = []
-i = 0
-# for schemes in student_schemes.find_all("div"):
-#     # print("\n\n")
-#
-#     # print(f"What is {schemes.h5.text.strip()}?")
-#     if temp != "":
-#         temp = temp+f", {schemes.h5.text.strip()}"
-#     else:
-#         temp = temp + f"{schemes.h5.text.strip()}"
-#     if schemes.ul is not None:
-#         tempDict = {"tag": f"Data-{i}", "patterns": [
-#             f"What is {schemes.h5.text.strip()}?"
-#         ], "responses": f"{schemes.h5.text.strip()} {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ','')}'>{schemes.ul.li.a.text.strip()}</a>" }
-#     else:
-#         tempDict = {"tag": f"Data-{i}", "patterns": [
-#             f"What is {schemes.h5.text.strip()}?"
-#         ], "responses": f"{schemes.h5.text.strip()} {schemes.p.text.strip()}"}
+html_page = requests.get("https://www.aicte-india.org/schemes/students-development-schemes").text
 
-#     i += 1
-#     queAns.append(tempDict)
-#
-# tempDict = {"tag": f"Data-{i}", "patterns": [
-#             f"What are the available schemes for student ?"
-#         ], "responses": f"Available Schemes Are : {temp}"}
-#
-# i += 1
-# queAns.append(tempDict)
-#
+scrap = BeautifulSoup(html_page,"lxml")
+
+student_schemes = scrap.find("div",class_="schemes_html lzt_html")
+
+temp = ""
+queAns = []
+i = 0
+for schemes in student_schemes.find_all("div"):
+    # print("\n\n")
+
+    # print(f"What is {schemes.h5.text.strip()}?")
+    if temp != "":
+        temp = temp+f", {schemes.h5.text.strip()}"
+    else:
+        temp = temp + f"{schemes.h5.text.strip()}"
+    if schemes.ul is not None:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ','')}'>{schemes.ul.li.a.text.strip()}</a>" }
+    else:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()} {schemes.p.text.strip()}"}
+
+    i += 1
+    queAns.append(tempDict)
+
+tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What are the available schemes for student ?"
+        ], "responses": f"Available Schemes Are : {temp}"}
+
+i += 1
+queAns.append(tempDict)
+
 # print(queAns)
-#
+
 
 
 
@@ -85,10 +85,9 @@ scrap = BeautifulSoup(html_page,"lxml")
 
 student_schemes = scrap.find("div",class_="schemes_html lzt_html")
 temp=""
-queans=[]
+# queans=[]
 # i=0
 for schemes in student_schemes.find_all("div"):
-    print(f"What is {schemes.h5.text.strip()}?")
     if temp != "":
         temp = temp+f", {schemes.h5.text.strip()}"
     else:
@@ -98,26 +97,23 @@ for schemes in student_schemes.find_all("div"):
         tempDict = {"tag": f"Data-{i}", "patterns": [
             f"What is {schemes.h5.text.strip()}?"
         ],
-                    "responses": f"{schemes.h5.text.strip()} {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ', '')}'>{schemes.ul.li.a.text.strip()}</a>"}
+                    "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ', '')}'>{schemes.ul.li.a.text.strip()}</a>"}
     else:
         tempDict = {"tag": f"Data-{i}", "patterns": [
             f"What is {schemes.h5.text.strip()}?"
         ], "responses": f"{schemes.h5.text.strip()} {schemes.p.text.strip()}"}
-    # print(schemes.p.text.strip())
-    # for sub in schemes.ul.find_all("li"):
-    #     print(sub.a.text)
-    #     print(sub.a['href'].replace(" ", ""))
+
     i += 1
-    queans.append(tempDict)
+    queAns.append(tempDict)
 
 tempDict = {"tag": f"Data-{i}", "patterns": [
-                f"What are the available schemes for student ?"
+                f"What are the available schemes for faculty ?"
             ], "responses": f"Available Schemes Are : {temp}"}
 
 i += 1
-queans.append(tempDict)
+queAns.append(tempDict)
 
-print(queans)
+# print(queAns)
 
 
 
@@ -128,36 +124,91 @@ print(queans)
 
 # Institutional Development Scheme
 ##########################################################
-# html_page = requests.get("https://www.aicte-india.org/schemes/institutional-development-schemes").text
-#
-# scrap = BeautifulSoup(html_page,"lxml")
-#
-# student_schemes = scrap.find("div",class_="schemes_html lzt_html")
-#
-# for schemes in student_schemes.find_all("div"):
-#     print(schemes.h5.text.strip())
-#     print(schemes.p.text.strip())
-#     for sub in schemes.ul.find_all("li"):
-#         print(sub.a.text)
-#         print(sub.a['href'].replace(" ", ""))
+temp=""
+html_page = requests.get("https://www.aicte-india.org/schemes/institutional-development-schemes").text
+
+scrap = BeautifulSoup(html_page,"lxml")
+
+student_schemes = scrap.find("div",class_="schemes_html lzt_html")
+
+for schemes in student_schemes.find_all("div"):
+    # print(schemes.h5.text.strip())
+    # print(schemes.p.text.strip())
+    # for sub in schemes.ul.find_all("li"):
+    #     print(sub.a.text)
+    #     print(sub.a['href'].replace(" ", ""))
+
+
+    if temp != "":
+        temp = temp+f", {schemes.h5.text.strip()}"
+    else:
+        temp = temp + f"{schemes.h5.text.strip()}"
+
+    if schemes.ul is not None:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ','')}'>{schemes.ul.li.a.text.strip()}</a>" }
+    else:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()}"}
+
+    i += 1
+    queAns.append(tempDict)
+
+tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What are the available schemes for Institutional Development ?"
+        ], "responses": f"Available Schemes Are : {temp}"}
+
+i += 1
+queAns.append(tempDict)
+
+# print(queAns)
 
 
 
 
 # Research & Innovations Development Schemes
 ##########################################################
-# html_page = requests.get("https://www.aicte-india.org/schemes/research-innovations-development-schemes").text
-#
-# scrap = BeautifulSoup(html_page,"lxml")
-#
-# student_schemes = scrap.find("div",class_="schemes_html lzt_html")
-#
-# for schemes in student_schemes.find_all("div"):
-#     print(schemes.h5.text.strip())
-#     print(schemes.p.text.strip())
-#     for sub in schemes.ul.find_all("li"):
-#         print(sub.a.text)
-#         print(sub.a['href'].replace(" ", ""))
+temp=""
+html_page = requests.get("https://www.aicte-india.org/schemes/research-innovations-development-schemes").text
+
+scrap = BeautifulSoup(html_page,"lxml")
+
+student_schemes = scrap.find("div",class_="schemes_html lzt_html")
+
+for schemes in student_schemes.find_all("div"):
+    # print(schemes.h5.text.strip())
+    # print(schemes.p.text.strip())
+    # for sub in schemes.ul.find_all("li"):
+    #     print(sub.a.text)
+    #     print(sub.a['href'].replace(" ", ""))
+
+    if temp != "":
+        temp = temp+f", {schemes.h5.text.strip()}"
+    else:
+        temp = temp + f"{schemes.h5.text.strip()}"
+
+    if schemes.ul is not None:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ','')}'>{schemes.ul.li.a.text.strip()}</a>" }
+    else:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()}"}
+
+    i += 1
+    queAns.append(tempDict)
+
+tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What are the available schemes for Research & Innovations Development Schemes ?"
+        ], "responses": f"Available Schemes Are : {temp}"}
+
+i += 1
+queAns.append(tempDict)
+
+# print(queAns)
 
 
 
@@ -165,39 +216,92 @@ print(queans)
 
 # General Schemes
 ##########################################################
-# html_page = requests.get("https://www.aicte-india.org/schemes/other-schemes").text
-#
-# scrap = BeautifulSoup(html_page,"lxml")
-#
-# student_schemes = scrap.find("div",class_="schemes_html lzt_html")
-#
-# for schemes in student_schemes.find_all("div"):
-#     print(schemes.h5.text.strip())
-#     print(schemes.p.text.strip())
-#     for sub in schemes.ul.find_all("li"):
-#         print(sub.a.text)
-#         print(sub.a['href'].replace(" ", ""))
+temp=""
+html_page = requests.get("https://www.aicte-india.org/schemes/other-schemes").text
+
+scrap = BeautifulSoup(html_page,"lxml")
+
+student_schemes = scrap.find("div",class_="schemes_html lzt_html")
+
+for schemes in student_schemes.find_all("div"):
+    # print(schemes.h5.text.strip())
+    # print(schemes.p.text.strip())
+    # for sub in schemes.ul.find_all("li"):
+    #     print(sub.a.text)
+    #     print(sub.a['href'].replace(" ", ""))
+
+    if temp != "":
+        temp = temp+f", {schemes.h5.text.strip()}"
+    else:
+        temp = temp + f"{schemes.h5.text.strip()}"
+
+    if schemes.ul is not None:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()} For More Information : <a href = '{schemes.ul.li.a['href'].replace(' ','')}'>{schemes.ul.li.a.text.strip()}</a>" }
+    else:
+        tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What is {schemes.h5.text.strip()}?"
+        ], "responses": f"{schemes.h5.text.strip()}. {schemes.p.text.strip()}"}
+
+    i += 1
+    queAns.append(tempDict)
+
+tempDict = {"tag": f"Data-{i}", "patterns": [
+            f"What are the available schemes for General Schemes?"
+        ], "responses": f"Available Schemes Are : {temp}"}
+
+i += 1
+queAns.append(tempDict)
+
+print(queAns)
 
 
 
 
 # Oppertunities for students
 ######################################################################################################
-# html_page = requests.get("https://www.aicte-india.org/opportunities/students/overview").text
-#
-# scrap = BeautifulSoup(html_page, "lxml")
-#
-# student_opp = scrap.find('section', id = "col_nats")
-#
-#
-# for opps in student_opp.find_all("div"):
-#     for subopp in opps.find_all("article"):
-#         print(subopp.h3.text)
-#         if subopp.find("p") is not None:
-#             print(subopp.p.text)
-#         # print(opps.p.text)
-#         print(subopp.a['href'].replace(" ",""))
-#         print(subopp.a.text)
+html_page = requests.get("https://www.aicte-india.org/opportunities/students/overview").text
+
+scrap = BeautifulSoup(html_page, "lxml")
+
+student_opp = scrap.find('section', id = "col_nats")
+temp=""
+
+for opps in student_opp.find_all("div"):
+    for subopp in opps.find_all("article"):
+        # print(subopp.h3.text)
+        # if subopp.find("p") is not None:
+        #     print(subopp.p.text)
+        # print(opps.p.text)
+        # print(subopp.a['href'].replace(" ",""))
+        # print(subopp.a.text)
+        if temp != "":
+            temp = temp + f", {subopp.h3.text}"
+        else:
+            temp = temp + f"{subopp.h3.text}"
+
+        if subopp.find("p") is not None:
+            tempDict = {"tag": f"Data-{i}", "patterns": [
+                f"What is {subopp.h3.text}?"
+            ],
+                        "responses": f"{subopp.h3.text}. {opps.p.text} For More Information : <a href = '{subopp.a['href'].replace(' ', '')}'>{subopp.a.text}</a>"}
+        else:
+            tempDict = {"tag": f"Data-{i}", "patterns": [
+                f"What is {subopp.h3.text}?"
+            ], "responses": f"{subopp.h3.text}. {opps.p.text}"}
+
+        i += 1
+        queAns.append(tempDict)
+
+    tempDict = {"tag": f"Data-{i}", "patterns": [
+        f"What are the Oppertunities for students?"
+    ], "responses": f"Available Oppertunities Are : {temp}"}
+
+    i += 1
+    queAns.append(tempDict)
+
+    print(queAns)
 
 
 
