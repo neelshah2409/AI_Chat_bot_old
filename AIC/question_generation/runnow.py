ff7929d815be728398916efa1d657a2f8ba19f36
Rishi Patel <patelrishi382@gmail.com>
2022-07-30 23:25:43 +0530

    Some bug fixes and encoding applied

--- AIC/question_generation/runnow.py
+++ AIC/question_generation/runnow.py
@@ -11,7 +11,7 @@ except Exception as e:
 
 def write_json(data,id):
     filename = f"{os.getcwd()}{os.sep}AIC_APP{os.sep}static{os.sep}AIC_APP{os.sep}intents{os.sep}intents{id}.json"
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf8") as f:
         json.dump(data, f, indent=4)
 
 
