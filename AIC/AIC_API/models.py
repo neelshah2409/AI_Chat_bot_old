919454cfaab02d8380f04ee70d92d6e0ff2a178b
Harshil Khamar <harshilkhamar1@gmail.com>
2022-08-01 10:36:30 +0530

    API models foreignkey fixed

--- AIC/AIC_API/models.py
+++ AIC/AIC_API/models.py
@@ -4,7 +4,7 @@ from rest_framework_api_key.models import APIKey
 
 class Api(models.Model):
     user_id = models.ForeignKey("AIC_APP.Yobotuser", on_delete=models.CASCADE)
-    api_key_id = models.ForeignKey("rest_framework_api_key.APIKey",primary_key=True, on_delete=models.CASCADE)
+    api_key_id = models.ForeignKey("rest_framework_api_key.APIKey",unique=True, on_delete=models.CASCADE)
     api_key = models.SlugField(max_length=255,unique=True)
     active = models.BooleanField(default=False)
     name = models.TextField(max_length=255,default="Public Api")
