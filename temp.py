7be05d22239a9f03fed8c9d67283d5addb03ec8c
Harshil Khamar <harshilkhamar1@gmail.com>
2022-08-03 23:31:22 +0530

    temporarily urls fixed (API)

--- temp.py
+++ temp.py
@@ -1,23 +1,28 @@
-# import docx
-#
-# doc = docx.Document("/Users/harshilkhamar/Desktop/temp.docx")
-# print(list(doc.paragraphs)[0].text.split("\n\n"))
-# # for i in f.read().split("\n\n"):
-# #     print(question)
-# #     if question:
-# #         questions.append(i.encode("utf8", errors="replace"))
-# #         question = not question
-# #     else:
-# #         answers.append(i.encode("utf8", errors="replace"))
-# #         question = not question
+# # import docx
 # #
-# # if(len(questions) == len(answers)):
-# #     print(questions,answers)
-# # else:
-# #     pass
+# # doc = docx.Document("/Users/harshilkhamar/Desktop/temp.docx")
+# # print(list(doc.paragraphs)[0].text.split("\n\n"))
+# # # for i in f.read().split("\n\n"):
+# # #     print(question)
+# # #     if question:
+# # #         questions.append(i.encode("utf8", errors="replace"))
+# # #         question = not question
+# # #     else:
+# # #         answers.append(i.encode("utf8", errors="replace"))
+# # #         question = not question
+# # #
+# # # if(len(questions) == len(answers)):
+# # #     print(questions,answers)
+# # # else:
+# # #     pass
+# #
+# #
+# #
 #
-#
-#
+# from rest_framework_api_key.crypto import KeyGenerator
+# print(KeyGenerator.generate())
 
-from rest_framework_api_key.crypto import KeyGenerator
-print(KeyGenerator.generate())
+from googletrans import Translator,LANGUAGES
+
+translator = Translator()
+print(LANGUAGES)
